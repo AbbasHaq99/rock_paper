de22acdce7d8b5c5173ae70d4f6e35684df53592
Abbas <[haqabbas112@gmail.com]>
2024-08-21 10:32:33 -0500

    Done Rock, Paper, Scissors

---
+++ test.js
@@ -0,0 +1,11 @@
+//make a function that getComputerChoice()
+function getComputerChoice(){
+    return ["Rock", "Paper", "Scissors"][Math.floor(Math.random() * 3)];
+}
+
+
+
+//ask the user rock paper or scissors
+
+//make condtions for the outcome
+
